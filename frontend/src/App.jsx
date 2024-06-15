af193757d1f7082f867634640494fbff46184bc7
rafatkab <rafat.kabir301@gmail.com>
2024-06-15 13:05:49 -0400

    created general codebase

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
