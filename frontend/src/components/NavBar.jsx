738bfb153efd4210d5f067754f02cd6aeff14264
rafatkab <rafat.kabir301@gmail.com>
2024-06-22 14:52:28 -0400

    Added tailwind compatability

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="w-screen h-16 border flex justify-end items-center">
+      <div className="flex p-8 [&>*]:p-3">
+        <div>Home</div>
+        <div>Our Mission</div>
+        <div>Course Description</div>
+        <div>Our Team</div>
+        <div>FAQ</div>
+      </div>
+    </div>
+  );
 }
