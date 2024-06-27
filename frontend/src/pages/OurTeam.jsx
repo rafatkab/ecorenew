c2d13481478438847a04bc70693cee77fc15ea32
Jordi Nakahara <jordi.nakahara@gmail.com>
2024-06-27 18:46:22 -0400

    Update OurTeam.jsx
    Added the basic text. Still needs the image, style, and that link at the bottom to the instagram.

--- frontend/src/pages/OurTeam.jsx
+++ frontend/src/pages/OurTeam.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function OurTeam() {
-  return <div>Our Team</div>;
+  return <div>
+    <div class="text-4xl text-center">Meet the Team</div>
+    <div class="pt-10 text-center">We worked on leveraging connection and social resilience in Bruce's Mill Conservation Park to improve wellness outcomes for our Summer Capstone Project.</div>
+  </div>;
 }
