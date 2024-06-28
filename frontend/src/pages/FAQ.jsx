a53c4a9a68587709bc7641a676e63a377245846c
Jordi Nakahara <jordi.nakahara@gmail.com>
2024-06-28 11:19:33 -0400

    Update FAQ.jsx
    Added the text to the page. Is still missing style, the dropdowns for the answers to the questions, and a link at the "here" in the answer to the last question.

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function FAQ() {
-  return <div>FAQ</div>;
+  return <div>
+    <div class="text-4xl text-center">Frequently Asked Questions</div>
+    <div class="text-2xl pt-20 px-20">Who are we partnered with?</div>
+    <div class="text pt-3 px-20">Our partners are York University, Toronto and Region Conservation Authority, and NewMakeIt in bridging education with hands-on learning.</div>
+    <div class="text-2xl pt-20 px-20">Why should I sign up?</div>
+    <div class="text pt-3 px-20">You will gain a 3 credit course at York University and develop your creativity and skills in restoring elements of  Bruce's Mill Conservation Park.</div>
+    <div class="text-2xl pt-20 px-20">Who can I reach out to?</div>
+    <div class="text pt-3 px-20">You can reach out to us here!</div>
+
+  </div>;
 }
