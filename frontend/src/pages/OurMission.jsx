000cf0937b94cf889db99357bd997a60e5a191ca
Jordi Nakahara <jordi.nakahara@gmail.com>
2024-07-11 14:18:20 -0400

    Formatting changes
    Centered the image in the "Our Team" page. Formatted text in "Our Mission" page.

--- frontend/src/pages/OurMission.jsx
+++ frontend/src/pages/OurMission.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 export default function OurMission() {
   return <div>
-    <div class="text-4xl text-center">Our Mission</div>
-    <div class="text-xl pt-10 px-20">Our Mission Statement</div>
-    <div class="pt-5 px-20">Our mission is to foster a deep connection between the youth of Stouffville and the natural environment. We aim to inspire and engage young people by providing meaningful opportunities for them to explore, appreciate, and actively participate in preserving the natural beauty of their community. Our two-week course unites York University students, experienced faculty, and community partners—Bruce’s Mill and TRCA—for collaborative, hands-on learning focused on renewing and revitalizing the outdoors. </div>
-    <div class="text-xl pt-10 px-20">What We Aim to Offer the Youth</div>
-    <div class="pt-5 px-20">At EcoRenew, we offer a hands-on course blending woodworking with cyanotype art to spark creativity and teach practical life skills. Participants learn to use carpenter tools, manage stress, and improve communication and teamwork skills. </div>
-    <div class="text-xl pt-10 px-20">Sustainable Development Goals</div>
-    <div class="pt-5 px-20">We are dedicated to creating something we believe in through the inclusivity of ideas and fostering social impact. </div>
+    <div className="font-body">
+      <div class="text-4xl text-center">OUR MISSION</div>
+    </div>
+
+    <div class="flex flex-col text-3xl font-medium pt-10 px-20">Our Mission Statement</div>
+    <div class="text-lg pt-5 px-20">Our mission is to foster a deep connection between the youth of Stouffville and the natural environment. We aim to inspire and engage young people by providing meaningful opportunities for them to explore, appreciate, and actively participate in preserving the natural beauty of their community. Our two-week course unites York University students, experienced faculty, and community partners—Bruce’s Mill and TRCA—for collaborative, hands-on learning focused on renewing and revitalizing the outdoors. </div>
+    <div class="flex flex-col text-3xl font-medium pt-10 px-20">What We Aim to Offer the Youth</div>
+    <div class="text-lg pt-5 px-20">At EcoRenew, we offer a hands-on course blending woodworking with cyanotype art to spark creativity and teach practical life skills. Participants learn to use carpenter tools, manage stress, and improve communication and teamwork skills. </div>
+    <div class="flex flex-col text-3xl font-medium pt-10 px-20">Sustainable Development Goals</div>
+    <div class="text-lg pt-5 px-20">We are dedicated to creating something we believe in through the inclusivity of ideas and fostering social impact. </div>
   </div>;
 }
--- frontend/src/pages/OurTeam.jsx
+++ frontend/src/pages/OurTeam.jsx
@@ -5,6 +5,8 @@ export default function OurTeam() {
   return <div>
     <div class="text-4xl text-center">Meet the Team</div>
     <div class="pt-10 text-center">We worked on leveraging connection and social resilience in Bruce's Mill Conservation Park to improve wellness outcomes for our Summer Capstone Project.</div>
-    <img src={teamPhoto} alt="team" class="center"/>
-  </div>;
+    <div className="flex justify-center p-10">
+        <img src={teamPhoto} />
+    </div>;
+  </div>
 }
