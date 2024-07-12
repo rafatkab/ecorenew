4cc640e09609eab351c0f4c684067d7789c17e64
Jordi Nakahara <jordi.nakahara@gmail.com>
2024-07-12 16:25:26 -0400

    Update OurMission.jsx
    Added the remaining image-text combo. Fixed the formatting to look more like the original.

--- frontend/src/pages/OurMission.jsx
+++ frontend/src/pages/OurMission.jsx
@@ -8,40 +8,47 @@ import sdgSeventeen from "../images/Our Mission/5f1b3c4fc2a1cf9cfdb7d73bc7a9273c
 export default function OurMission() {
   return <div>
     <div class="font-body text-4xl text-center">OUR MISSION</div>
-    
+
     <div class="flex flex-col text-3xl font-medium pt-10 px-20">Our Mission Statement</div>
     <div class="text-lg pt-5 px-20">Our mission is to foster a deep connection between the youth of Stouffville and the natural environment. We aim to inspire and engage young people by providing meaningful opportunities for them to explore, appreciate, and actively participate in preserving the natural beauty of their community. Our two-week course unites York University students, experienced faculty, and community partners—Bruce’s Mill and TRCA—for collaborative, hands-on learning focused on renewing and revitalizing the outdoors. </div>
-    
+
     <div class="flex flex-col text-3xl font-medium pt-10 px-20">What We Aim to Offer the Youth</div>
     <div class="text-lg pt-5 px-20">At EcoRenew, we offer a hands-on course blending woodworking with cyanotype art to spark creativity and teach practical life skills. Participants learn to use carpenter tools, manage stress, and improve communication and teamwork skills. </div>
-    
+
     <div class="flex flex-col text-3xl font-medium pt-10 px-20">Sustainable Development Goals</div>
     <div class="text-lg pt-5 px-20">We are dedicated to creating something we believe in through the inclusivity of ideas and fostering social impact. </div>
-    
-    <div class="grid grid-cols-6 pt-10 gap-4">
-    <img class="col-start-2" src={sdgThree}/>
-      <div>The park's natural environment generates positive emotions and acts as a form of ecotherapy
-         when activities involve nature. These activities promote wellness because they allow for 
+
+    <div class="grid grid-cols-6 pt-10 px-20 gap-4">
+      <img src={sdgThree} />
+      <div class="col-span-2">The park's natural environment generates positive emotions and acts as a form of ecotherapy
+        when activities involve nature. These activities promote wellness because they allow for
         effective practices for managing mental health through a creative outlet for students.</div>
-        <img src={sdgFour}/>
-      <div>As our program needs to train individuals to help run the course, our project offers 
-        employment opportunities to oversee the program and invite guest speakers from the 
-        community that focus on design planning, environmental sustainability, team building 
+      <img src={sdgFour} />
+      <div class="col-span-2">As our program needs to train individuals to help run the course, our project offers
+        employment opportunities to oversee the program and invite guest speakers from the
+        community that focus on design planning, environmental sustainability, team building
         and stress management which creates mentorship between the students. </div>
     </div>
 
-    <div class="grid grid-cols-6 pt-10 gap-4">
-      <img class="col-start-2" src={sdgEight}/>
-      <div>As our program needs to train individuals to help run the course, our project offers employment 
-        opportunities to oversee the program and invite guest speakers from the community that focus on 
-        design planning, environmental sustainability, team building and stress management which creates 
+    <div class="grid grid-cols-6 pt-10 px-20 gap-4">
+      <img src={sdgEight} />
+      <div class="col-span-2">As our program needs to train individuals to help run the course, our project offers employment
+        opportunities to oversee the program and invite guest speakers from the community that focus on
+        design planning, environmental sustainability, team building and stress management which creates
         mentorship between the students. </div>
-        <img src={sdgEleven}/>
-      <div>One target that this goal aims to achieve is to provide access to safe and inclusive natural 
-        spaces. Bringing this program in to make the park more accessible for activities such as 
-        rebuilding platforms for hiking trails or benches for seating can encourage more visitors to 
+      <img src={sdgEleven} />
+      <div class="col-span-2">One target that this goal aims to achieve is to provide access to safe and inclusive natural
+        spaces. Bringing this program in to make the park more accessible for activities such as
+        rebuilding platforms for hiking trails or benches for seating can encourage more visitors to
         come out into Bruce's Mill Conservation Park to enjoy and utilize the green space. </div>
     </div>
 
+    <div class="grid grid-cols-6 pt-10 px-20 gap-4">
+      <img src={sdgSeventeen} />
+      <div class="col-span-2">Our goal is to establish effective partnerships with the York University community,
+        including undergraduate and graduate students. We aim to develop a program that meets their academic requirements,
+        build relationships to increase involvement in the park, and network to ensure the program's sustainability for
+        future expansion into different demographics. </div>
+    </div>
   </div>;
 }
